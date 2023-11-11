9e197e2d77b86d7a09fe0608aba2138c398d8c26
woozygoozy <ilyav@gusinski.us>
2023-11-11 06:18:47 -0500

    patch: rollback the release.config.js

--- release.config.js
+++ release.config.js
@@ -1,62 +1,23 @@
 module.exports = {
     branches: [
-        'main', // Standard release branch
         {
-            name: 'beta', // Example prerelease branch
-            prerelease: true
-        },
-        {
-            name: 'alpha', // Your existing alpha prerelease branch
-            prerelease: true
-        }
-        // Add more branches as needed
+            type: 'prerelease',
+            name: 'main',
+            prerelease: 'alpha',
+        },  // Add the prerelease property here
     ],
     plugins: [
-        ['@semantic-release/commit-analyzer', {
-            preset: 'angular',
-            releaseRules: [
-                { type: 'docs', scope: 'README', release: 'patch', hidden: true },
-                { type: 'refactor', release: 'patch' },
-                { type: 'feat', release: 'minor' },
-                { type: 'fix', release: 'patch' },
-                // Add more custom rules as needed
-            ],
-        }],
-        ['@semantic-release/release-notes-generator', {
-            preset: 'angular',
-            writerOpts: {
-                transform: (commit, context) => {
-                    const isRelevantChange = (line) => line.startsWith('fix:') || line.startsWith('feat:');
-
-                    if (commit && commit.message) {
-                        const lines = commit.message.split('\n');
-                        const relevantLines = lines.filter(isRelevantChange);
-
-                        return relevantLines.map(line => {
-                            const [type, ...messageParts] = line.split(':');
-                            const message = messageParts.join(':').trim();
-
-                            return {
-                                ...commit,
-                                type,
-                                subject: message
-                            };
-                        });
-                    }
-
-                    return null;
-                }, 
-            }, 
-        }],
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
-        ['@semantic-release/git', {
-            assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
-            message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-        }],
-        ['@semantic-release/github', {
-            assets: 'dist/*.zip' // Adjust according to your build artifacts
-        }],
-        // Add more plugins as necessary
+        //'@semantic-release/npm',  // If you're publishing to npm
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+        '@semantic-release/github',
     ],
-    ci: false, //  Set to true if you are using CI
 };
